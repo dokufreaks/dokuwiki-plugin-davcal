4ed26d55151fd9e69648a114800407cdb2127288
Andreas Boehler <andreas@aboehler.at>
2015-10-20 21:39:03 +0200

    Add table output and ODT support by means of the table

---
+++ action/cache.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * DokuWiki DAVCal PlugIn - Ajax component
+ */
+
+if(!defined('DOKU_INC')) die();
+
+class action_plugin_davcal_cache extends DokuWiki_Action_Plugin {
+
+    /**
+     * @var helper_plugin_davcal
+     */
+    private $hlp = null;
+
+    function __construct() {
+        $this->hlp =& plugin_load('helper','davcal');
+    }
+
+    function register(Doku_Event_Handler $controller) {
+        $controller->register_hook('PARSER_CACHE_USE', 'BEFORE', $this, 'handle_parser_cache_use');
+    }
+
+    function handle_parser_cache_use(&$event, $param) {
+      global $ID;
+      $cache = &$event->data;
+      if(!isset($cache->page)) return;
+      
+      $davcalMeta = p_get_metadata($ID, 'plugin_davcal');
+      if(!$davcalMeta)
+        return;
+      
+      if(isset($davcalMeta['table']) && $davcalMeta['table'] === true)
+      {
+            $event->preventDefault();
+            $event->stopPropagation();
+            $event->result = false;
+      }         
+    }
+ 
+}              
--- syntax.php
+++ syntax.php
@@ -57,13 +57,20 @@ class syntax_plugin_davcal extends DokuWiki_Syntax_Plugin {
         global $ID;
         $options = trim(substr($match,9,-2));
         $options = explode(',', $options);
+
         $data = array('name' => $ID,
                       'description' => $this->getLang('created_by_davcal'),
                       'id' => array(),
                       'settings' => 'show',
-                      'view' => 'month'
+                      'view' => 'month',
+                      'table' => false,
+                      'startdate' => 'today',
+                      'numdays' => 30,
+                      'dateformat' => 'Y-m-d H:i',
+                      'onlystart' => false,
                       );
         $lastid = $ID;
+
         foreach($options as $option)
         {
             list($key, $val) = explode('=', $option);
@@ -85,19 +92,28 @@ class syntax_plugin_davcal extends DokuWiki_Syntax_Plugin {
                     else
                         $data['view'] = 'month';
                 break;
+                case 'table':
+                    $data['table'] = true;
+                break;
+                case 'onlystart':
+                    if(($val === 'on') || ($val === 'true'))
+                        $data['onlystart'] = true;
+                break;
                 default:
                     $data[$key] = $val;
             }
         }
+
         // Handle the default case when the user didn't enter a different ID
         if(empty($data['id']))
         {
             $data['id'] = array($ID => '#3a87ad');
         }
         // Only update the calendar name/description if the ID matches the page ID.
+        // Disable this, if we output to table. 
         // Otherwise, the calendar is included in another page and we don't want
         // to interfere with its data.
-        if(in_array($ID, array_keys($data['id'])))
+        if(in_array($ID, array_keys($data['id'])) && ($data['table'] === false))
         {
             if(isset($_SERVER['REMOTE_USER']) && !is_null($_SERVER['REMOTE_USER']))
                 $username = $_SERVER['REMOTE_USER'];
@@ -116,26 +132,89 @@ class syntax_plugin_davcal extends DokuWiki_Syntax_Plugin {
      * Create output
      */
     function render($format, &$R, $data) {
-        if($format != 'xhtml') return false;
+        if(($format != 'xhtml') && ($format != 'odt')) return false;
         global $ID;
-        $tzlist = \DateTimeZone::listIdentifiers(DateTimeZone::ALL);
-        
-        // Render the Calendar. Timezone list is within a hidden div,
-        // the calendar ID is in a data-calendarid tag.
-        $R->doc .= '<div id="fullCalendar" data-calendarpage="'.$ID.'"></div>';
-        $R->doc .= '<div id="fullCalendarTimezoneList" class="fullCalendarTimezoneList" style="display:none">';
-        $R->doc .= '<select id="fullCalendarTimezoneDropdown">';
-        $R->doc .= '<option value="local">'.$this->getLang('local_time').'</option>';
-        foreach($tzlist as $tz)
+                
+        // Handle creating a table
+        // For ODT output, always create the table as the calendar doesn't make sense
+        if(($data['table'] === true) || ($format == 'odt'))
         {
-            $R->doc .= '<option value="'.$tz.'">'.$tz.'</option>';
+            $events = array();
+            $from = $data['startdate'];
+            if($from === 'today')
+                $from = new \DateTime();
+            else
+                $from = new \DateTime($from);
+            $to = clone $from;
+            $to->add(new \DateInterval('P'.$data['numdays'].'D'));
+            foreach($data['id'] as $calPage => $color)
+            {
+                $events = array_merge($events, $this->hlp->getEventsWithinDateRange($calPage, 
+                                          $user, $from->format('Y-m-d'), $to->format('Y-m-d'))); 
+            }
+            $R->table_open();
+            $R->tablethead_open();
+            $R->tableheader_open();
+            $R->doc .= $data['onlystart'] ? $this->getLang('at') : $this->getLang('from');
+            $R->tableheader_close();
+            if(!$data['onlystart'])
+            {
+                $R->tableheader_open();
+                $R->doc .= $this->getLang('to');
+                $R->tableheader_close();
+            }
+            $R->tableheader_open();
+            $R->doc .= $this->getLang('title');
+            $R->tableheader_close();
+            $R->tableheader_open();
+            $R->doc .= $this->getLang('description');
+            $R->tableheader_close();
+            $R->tablethead_close();
+            foreach($events as $event)
+            {
+                $R->tablerow_open();
+                $R->tablecell_open();
+                $from = new \DateTime($event['start']);
+                $to = new \DateTime($event['end']);
+                $R->doc .= $from->format($data['dateformat']);
+                $R->tablecell_close();
+                if(!$data['onlystart'])
+                {
+                    $R->tablecell_open();
+                    $R->doc .= $to->format($data['dateformat']);
+                    $R->tablecell_close();
+                }
+                $R->tablecell_open();
+                $R->doc .= $event['title'];
+                $R->tablecell_close();
+                $R->tablecell_open();
+                $R->doc .= $event['description'];
+                $R->tablecell_close();
+                $R->tablerow_close();
+            }
+            $R->table_close();
         }
-        $R->doc .= '</select></div>';
-        if(($this->getConf('hide_settings') !== 1) && ($data['settings'] !== 'hide'))
-        {            
-            $R->doc .= '<div class="fullCalendarSettings"><a href="#" class="fullCalendarSettings"><img src="'.DOKU_URL.'lib/plugins/davcal/images/settings.png'.'">'.$this->getLang('settings').'</a></div>';
+        // Handle JavaScript calendar output
+        else
+        {
+            $tzlist = \DateTimeZone::listIdentifiers(DateTimeZone::ALL);
+            
+            // Render the Calendar. Timezone list is within a hidden div,
+            // the calendar ID is in a data-calendarid tag.
+            $R->doc .= '<div id="fullCalendar" data-calendarpage="'.$ID.'"></div>';
+            $R->doc .= '<div id="fullCalendarTimezoneList" class="fullCalendarTimezoneList" style="display:none">';
+            $R->doc .= '<select id="fullCalendarTimezoneDropdown">';
+            $R->doc .= '<option value="local">'.$this->getLang('local_time').'</option>';
+            foreach($tzlist as $tz)
+            {
+                $R->doc .= '<option value="'.$tz.'">'.$tz.'</option>';
+            }
+            $R->doc .= '</select></div>';
+            if(($this->getConf('hide_settings') !== 1) && ($data['settings'] !== 'hide'))
+            {            
+                $R->doc .= '<div class="fullCalendarSettings"><a href="#" class="fullCalendarSettings"><img src="'.DOKU_URL.'lib/plugins/davcal/images/settings.png'.'">'.$this->getLang('settings').'</a></div>';
+            }
         }
-     
     }
 
 
