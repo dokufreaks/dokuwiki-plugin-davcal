5b6f6d95f860fc35ab2e2f1653126f605642809a
Andreas Boehler <andreas@aboehler.at>
2015-11-12 20:16:46 +0100

    Update Dutch translation

---
+++ lang/nl/settings.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['hide_settings'] = 'Verberg de instellings-knop onder de kalender';
+$lang['disable_sync'] = 'CalDAV synchronisatie uitschakelen (prive ICS URL ongemoeid laten)';
+$lang['disable_ics'] = 'Prive ICS URL synchronisatie uitschakalen (CalDAV synchronisatie ongemoeid laten)';
+$lang['monday'] = 'Week begint op maandag in plaats van zondag';
+$lang['timezone'] = 'Tijdzone van de kalender (bijvoorbeeld "Europe/Vienna" of "local" (van browser))';
+$lang['workweek'] = 'Toon alleen werkdagen';
+$lang['weeknumbers'] = 'Toon weeknummers';
+~
+
