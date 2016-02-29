f8d0d533219b23c44d6498a0f570a03c31d5aa7e
Andreas Boehler <andreas@aboehler.at>
2016-02-29 20:48:37 +0100

    Update French translation

--- lang/fr/settings.php
+++ lang/fr/settings.php
@@ -4,10 +4,10 @@
  * 
  * @author Digitalin
  */
-$lang['hide_settings'] = 'Hide the settings button beneath the calendar';
-$lang['disable_sync'] = 'Disable CalDAV synchronisation (but still allow private ICS URL)';
-$lang['disable_ics'] = 'Disable private ICS URL synchronisation (but still allow CalDAV sync)';
-$lang['monday'] = 'Start week on Monday instead of Sunday';
-$lang['timezone'] = 'Set the timezone of the calendar (e.g. "Europe/Vienna" or "local" for browser-based)';
-$lang['workweek'] = 'Show only workweek';
-$lang['weeknumbers'] = 'Show week numbers';
+​$lang['hide_settings'] = 'Cacher le bouton de configuration sous le calendrier';
+$lang['disable_sync'] = 'Désactiver la synchronisation CalDAV (tout en conservant ICS URL privé';
+$lang['disable_ics'] = 'Désactiver la synchronisation ICS URL privé (tout en conservant la synchronisation CalDAV)';
+$lang['monday'] = 'Commencer la semaine le lundi au lieu du dimanche';
+$lang['timezone'] = 'Régler le fuseau horaire du calendrier  (e.g. "Europe/Vienna" ou "local" basé sur le navigateur)';
+$lang['workweek'] = 'Afficher seulement la semaine';
+$lang['weeknumbers'] = 'Afficher les numéros de semaine';
