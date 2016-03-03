12de568f2dfec9bf9c547d6637e12d5256990256
Andreas Boehler <andreas@aboehler.at>
2016-03-03 17:45:34 +0100

    Fix french translation

--- lang/fr/settings.php
+++ lang/fr/settings.php
@@ -4,8 +4,8 @@
  * 
  * @author Digitalin
  */
-​$lang['hide_settings'] = 'Cacher le bouton de configuration sous le calendrier';
-$lang['disable_sync'] = 'Désactiver la synchronisation CalDAV (tout en conservant ICS URL privé';
+$lang['hide_settings'] = 'Cacher le bouton de configuration sous le calendrier';
+$lang['disable_sync'] = 'Désactiver la synchronisation CalDAV (tout en conservant ICS URL privé)';
 $lang['disable_ics'] = 'Désactiver la synchronisation ICS URL privé (tout en conservant la synchronisation CalDAV)';
 $lang['monday'] = 'Commencer la semaine le lundi au lieu du dimanche';
 $lang['timezone'] = 'Régler le fuseau horaire du calendrier  (e.g. "Europe/Vienna" ou "local" basé sur le navigateur)';
